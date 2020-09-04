757b52607916a7630f52631e097b45bc4853ad55
Hans Pagel <hans.pagel@ueber.io>
2020-09-04 14:47:17 +0200

    add basic unit test setup

---
+++ tests/cypress/integration/core/generateHtml.spec.js
@@ -0,0 +1,8 @@
+// any import breaks
+import { getSchema } from '@tiptap/core'
+
+describe('example test', () => {
+  it('should work', () => {
+    expect('<p>Example Text</p>').to.eq('<p>Example Text</p>')
+  })
+})
