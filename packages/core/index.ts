8ff62cb33c5107742275b9c4d052b1498cab73a0
Hans Pagel <hans.pagel@ueber.io>
2020-09-08 17:32:44 +0200

    get typescript tests working

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -8,6 +8,8 @@ export { default as Node } from './src/Node'
 export { default as Mark } from './src/Mark'
 export { Extensions } from './src/types'
 
+export { default as capitalize } from './src/utils/capitalize'
+
 export { default as markInputRule } from './src/inputRules/markInputRule'
 export { default as markPasteRule } from './src/pasteRules/markPasteRule'
 
