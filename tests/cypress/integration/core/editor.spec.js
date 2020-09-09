271ec2f9d3041041833b98b258f1aef2041b60c0
Hans Pagel <hans.pagel@ueber.io>
2020-09-09 13:18:56 +0200

    add one example editor unit test

---
+++ tests/cypress/integration/core/editor.spec.js
@@ -0,0 +1,19 @@
+import { Editor } from '@tiptap/core'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+
+describe('editor', () => {
+  it('sets an initial content from HTML', () => {
+    const editor = new Editor({
+      content: '<p>Example Text</p>',
+      extensions: [
+        new Document(),
+        new Paragraph(),
+        new Text(),
+      ],
+    })
+
+    expect(editor.html()).to.eq('<p>Example Text</p>')
+  })
+})
