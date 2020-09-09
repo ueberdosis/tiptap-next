d6773b1ba0701905168c725ab9397e0be4dfb13a
Hans Pagel <hans.pagel@ueber.io>
2020-09-09 16:34:33 +0200

    add eslint prettier to the setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
-  extends: ['plugin:md/recommended'],
+  extends: ['plugin:prettier/recommended', 'plugin:md/recommended'],
   overrides: [
     {
       files: ['*.md'],
       parser: 'markdown-eslint-parser',
+      rules: {
+        'prettier/prettier': [
+          'error',
+          // Important to force prettier to use "markdown" parser - otherwise it wouldn't be able to parse *.md files.
+          // You also can configure other options supported by prettier here - "prose-wrap" is
+          // particularly useful for *.md files
+          { parser: 'markdown' },
+        ],
+      },
     },
   ],
 }
