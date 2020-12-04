d57c20e98f1a09d8f66230d13d5e7ff1fdfc1fae
Florian Wiech <git@florianwiech.me>
2020-12-04 16:59:57 +0100

    refactor: extension-history

---
+++ packages/extension-history/src/history.ts
@@ -0,0 +1,53 @@
+import { Command, Extension } from '@tiptap/core'
+import { history, undo, redo } from 'prosemirror-history'
+
+export interface HistoryOptions {
+  depth: number,
+  newGroupDelay: number,
+}
+
+export const History = Extension.create({
+  name: 'history',
+
+  defaultOptions: <HistoryOptions>{
+    depth: 100,
+    newGroupDelay: 500,
+  },
+
+  addCommands() {
+    return {
+      /**
+       * Undo recent changes
+       */
+      undo: (): Command => ({ state, dispatch }) => {
+        return undo(state, dispatch)
+      },
+      /**
+       * Reapply reverted changes
+       */
+      redo: (): Command => ({ state, dispatch }) => {
+        return redo(state, dispatch)
+      },
+    }
+  },
+
+  addProseMirrorPlugins() {
+    return [
+      history(this.options),
+    ]
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      'Mod-z': () => this.editor.commands.undo(),
+      'Mod-y': () => this.editor.commands.redo(),
+      'Shift-Mod-z': () => this.editor.commands.redo(),
+    }
+  },
+})
+
+declare module '@tiptap/core' {
+  interface AllExtensions {
+    History: typeof History,
+  }
+}
--- packages/extension-history/src/index.ts
+++ packages/extension-history/src/index.ts
@@ -1,55 +1,5 @@
-import { Command, Extension } from '@tiptap/core'
-import { history, undo, redo } from 'prosemirror-history'
+import { History } from './history'
 
-export interface HistoryOptions {
-  depth: number,
-  newGroupDelay: number,
-}
-
-const History = Extension.create({
-  name: 'history',
-
-  defaultOptions: <HistoryOptions>{
-    depth: 100,
-    newGroupDelay: 500,
-  },
-
-  addCommands() {
-    return {
-      /**
-       * Undo recent changes
-       */
-      undo: (): Command => ({ state, dispatch }) => {
-        return undo(state, dispatch)
-      },
-      /**
-       * Reapply reverted changes
-       */
-      redo: (): Command => ({ state, dispatch }) => {
-        return redo(state, dispatch)
-      },
-    }
-  },
-
-  addProseMirrorPlugins() {
-    return [
-      history(this.options),
-    ]
-  },
-
-  addKeyboardShortcuts() {
-    return {
-      'Mod-z': () => this.editor.commands.undo(),
-      'Mod-y': () => this.editor.commands.redo(),
-      'Shift-Mod-z': () => this.editor.commands.redo(),
-    }
-  },
-})
+export * from './history'
 
 export default History
-
-declare module '@tiptap/core' {
-  interface AllExtensions {
-    History: typeof History,
-  }
-}
