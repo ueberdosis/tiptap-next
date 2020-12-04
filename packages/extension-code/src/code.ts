50b462653c868d825387e28685f12561e5c31232
Florian Wiech <git@florianwiech.me>
2020-12-04 16:39:00 +0100

    refactor: extension-code

---
+++ packages/extension-code/src/code.ts
@@ -0,0 +1,83 @@
+import {
+  Command,
+  Mark,
+  markInputRule,
+  markPasteRule,
+  mergeAttributes,
+} from '@tiptap/core'
+
+export interface CodeOptions {
+  HTMLAttributes: {
+    [key: string]: any
+  },
+}
+
+export const inputRegex = /(?:^|\s)((?:`)((?:[^`]+))(?:`))$/gm
+export const pasteRegex = /(?:^|\s)((?:`)((?:[^`]+))(?:`))/gm
+
+export const Code = Mark.create({
+  name: 'code',
+
+  defaultOptions: <CodeOptions>{
+    HTMLAttributes: {},
+  },
+
+  excludes: '_',
+
+  parseHTML() {
+    return [
+      { tag: 'code' },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['code', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
+  },
+
+  addCommands() {
+    return {
+      /**
+       * Set a code mark
+       */
+      setCode: (): Command => ({ commands }) => {
+        return commands.setMark('code')
+      },
+      /**
+       * Toggle inline code
+       */
+      toggleCode: (): Command => ({ commands }) => {
+        return commands.toggleMark('code')
+      },
+      /**
+       * Unset a code mark
+       */
+      unsetCode: (): Command => ({ commands }) => {
+        return commands.unsetMark('code')
+      },
+    }
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      'Mod-e': () => this.editor.commands.toggleCode(),
+    }
+  },
+
+  addInputRules() {
+    return [
+      markInputRule(inputRegex, this.type),
+    ]
+  },
+
+  addPasteRules() {
+    return [
+      markPasteRule(inputRegex, this.type),
+    ]
+  },
+})
+
+declare module '@tiptap/core' {
+  interface AllExtensions {
+    Code: typeof Code,
+  }
+}
