1a1b99526bcaa82f8c53d35d97e06ab31fc0d40b
Florian Wiech <git@florianwiech.me>
2020-12-04 16:34:06 +0100

    refactor: extension-buttlet-list

---
+++ packages/extension-bullet-list/src/bullet-list.ts
@@ -0,0 +1,61 @@
+import { Command, Node, mergeAttributes } from '@tiptap/core'
+import { wrappingInputRule } from 'prosemirror-inputrules'
+
+export interface BulletListOptions {
+  HTMLAttributes: {
+    [key: string]: any
+  },
+}
+
+export const inputRegex = /^\s*([-+*])\s$/
+
+export const BulletList = Node.create({
+  name: 'bulletList',
+
+  defaultOptions: <BulletListOptions>{
+    HTMLAttributes: {},
+  },
+
+  group: 'block list',
+
+  content: 'listItem+',
+
+  parseHTML() {
+    return [
+      { tag: 'ul' },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['ul', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
+  },
+
+  addCommands() {
+    return {
+      /**
+       * Toggle a bullet list
+       */
+      toggleBulletList: (): Command => ({ commands }) => {
+        return commands.toggleList('bulletList', 'listItem')
+      },
+    }
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      'Mod-Shift-8': () => this.editor.commands.toggleBulletList(),
+    }
+  },
+
+  addInputRules() {
+    return [
+      wrappingInputRule(inputRegex, this.type),
+    ]
+  },
+})
+
+declare module '@tiptap/core' {
+  interface AllExtensions {
+    BulletList: typeof BulletList,
+  }
+}
