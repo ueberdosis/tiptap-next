2785bc42b30637fcdd70eb96276dab6fba92bada
Florian Wiech <git@florianwiech.me>
2020-12-04 16:51:55 +0100

    refactor: extension-font-family

---
+++ packages/extension-font-family/src/font-family.ts
@@ -0,0 +1,67 @@
+import { Command, Extension } from '@tiptap/core'
+import '@tiptap/extension-text-style'
+
+type FontFamilyOptions = {
+  types: string[],
+}
+
+export const FontFamily = Extension.create({
+  name: 'fontFamily',
+
+  defaultOptions: <FontFamilyOptions>{
+    types: ['textStyle'],
+  },
+
+  addGlobalAttributes() {
+    return [
+      {
+        types: this.options.types,
+        attributes: {
+          fontFamily: {
+            default: null,
+            renderHTML: attributes => {
+              if (!attributes.fontFamily) {
+                return {}
+              }
+
+              return {
+                style: `font-family: ${attributes.fontFamily}`,
+              }
+            },
+            parseHTML: element => ({
+              fontFamily: element.style.fontFamily.replace(/['"]+/g, ''),
+            }),
+          },
+        },
+      },
+    ]
+  },
+
+  addCommands() {
+    return {
+      /**
+       * Set the font family
+       */
+      setFontFamily: (fontFamily: string): Command => ({ chain }) => {
+        return chain()
+          .setMark('textStyle', { fontFamily })
+          .run()
+      },
+      /**
+       * Unset the font family
+       */
+      unsetFontFamily: (): Command => ({ chain }) => {
+        return chain()
+          .setMark('textStyle', { fontFamily: null })
+          .removeEmptyTextStyle()
+          .run()
+      },
+    }
+  },
+})
+
+declare module '@tiptap/core' {
+  interface AllExtensions {
+    FontFamily: typeof FontFamily,
+  }
+}
