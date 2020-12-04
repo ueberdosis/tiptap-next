f93a420e0266ab763fcf67a0edfce312b8c02b95
Florian Wiech <git@florianwiech.me>
2020-12-04 16:56:28 +0100

    refactor: extension-hard-break

---
+++ packages/extension-hard-break/src/hard-break.ts
@@ -0,0 +1,66 @@
+import { Command, Node, mergeAttributes } from '@tiptap/core'
+import { exitCode } from 'prosemirror-commands'
+
+export interface HardBreakOptions {
+  HTMLAttributes: {
+    [key: string]: any
+  },
+}
+
+export const HardBreak = Node.create({
+  name: 'hardBreak',
+
+  defaultOptions: <HardBreakOptions>{
+    languageClassPrefix: 'language-',
+    HTMLAttributes: {},
+  },
+
+  inline: true,
+
+  group: 'inline',
+
+  selectable: false,
+
+  parseHTML() {
+    return [
+      { tag: 'br' },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['br', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)]
+  },
+
+  addCommands() {
+    return {
+      /**
+       * Add a hard break
+       */
+      setHardBreak: (): Command => ({ commands, state, dispatch }) => {
+        return commands.first([
+          () => exitCode(state, dispatch),
+          () => {
+            if (dispatch) {
+              state.tr.replaceSelectionWith(this.type.create()).scrollIntoView()
+            }
+
+            return true
+          },
+        ])
+      },
+    }
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      'Mod-Enter': () => this.editor.commands.setHardBreak(),
+      'Shift-Enter': () => this.editor.commands.setHardBreak(),
+    }
+  },
+})
+
+declare module '@tiptap/core' {
+  interface AllExtensions {
+    HardBreak: typeof HardBreak,
+  }
+}
